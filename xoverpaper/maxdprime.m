87aa5ec203c5fadee5f6bc67e9c18792489c3d06
David Fencsik <david.fencsik@gmail.com>
2007-06-14 01:00:42 +0000

    xoverpaper/:
    * maxdprime.m: added help text, made method matching case-insensitive,
      and fixed some non-array divisions (i.e., "/" to "./")

--- xoverpaper/maxdprime.m
+++ xoverpaper/maxdprime.m
@@ -1,5 +1,20 @@
 function [dprime, criterion] = maxdprime (hr, fa, n, method)
 
+% maxdprime calculates d' and criterion for a search task assuming
+% that response is based on the maximum of target and distractor
+% signals
+%
+% [dprime, criterion] = maxdprime(HitRate, FalseAlarmRate, SetSize)
+% computes d' and criterion based on the given hit rate, false alarm
+% rate, and set size.  If output arguments are omitted, then d' and
+% criterion will be printed on the command window.
+%
+% [dprime, criterion] = maxdprime(HitRate, FalseAlarmRate, SetSize, Method)
+% allows for different computational methods to be used (Method is a
+% case-insensitive string).  Currently, only the 'Palmer' method is
+% implemented (based on Palmer, Ames, & Lindsey, 1993).
+
+% Author: David Fencsik <fencsik@gmail.com>
 % $LastChangedDate$
 
 if nargin < 4
@@ -9,9 +24,9 @@ if nargin < 3
    error('at least three arguments are required');
 end
 
-if strcmp(method, 'palmer')
-   c = Finv((1 - fa) .^ (1 / n));
-   d = c - Finv((1 - hr) / (F(c) .^ (n - 1)));
+if strcmpi(method, 'palmer')
+   c = Finv((1 - fa) .^ (1 ./ n));
+   d = c - Finv((1 - hr) ./ (F(c) .^ (n - 1)));
 else
    error('requested method ''%s'' is not supported', num2str(method));
 end
@@ -25,8 +40,10 @@ elseif nargout >= 2
    criterion = c;
 end
 
+
 function p = F(x)
 p = normcdf(x, 0, 1);
+
 
 function x = Finv(p)
 x = norminv(p, 0, 1);
