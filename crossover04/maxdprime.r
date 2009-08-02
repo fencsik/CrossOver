6f0dac1c98cf039bf830fd47932c1e953bb1820a
David Fencsik <david.fencsik@gmail.com>
2009-08-02 15:11:41 -0700

    Add dprime calculation for max-rule search
    Calculates dprime and criterion correctly.  Currently missing
    error-checking on input arguments.

---
+++ crossover04/maxdprime.r
@@ -0,0 +1,33 @@
+### Calculate d' and criterion for a search task assuming that response is
+### based on the maximum of target and distractor signals.
+###
+### Currently, requires hit rate (hr), false-alarm rate (fa), and setsize
+### to be vectors of length N such that N is the same for each.  Capacity
+### can be left out, or set to any single value.
+
+maxdprime <- function (hr, fa, setsize, capacity=NULL) {
+
+    ## Need to add error-checking to ensure dimensions are correct
+
+    ## If no capacity given, then set capacity to max setsize, which
+    ## effectively means unlimited capacity
+    if (is.null(capacity) || capacity < 0) {
+        capacity <- max(setsize)
+        limitedCapacity <- FALSE
+    } else {
+        limitedCapacity <- TRUE
+    }
+
+    k <- pmin(setsize, capacity)
+    n <- setsize
+
+    crit <- qnorm((1 - fa) ^ (1 / k))
+    p <- pnorm(crit)
+    dp <- crit -
+        qnorm(n * (1 - hr) / k / (p ^ (k - 1)) - (n - k) / k * p)
+
+    if (!limitedCapacity)
+        capacity <- NULL
+
+    return(list(dprime=dp, criterion=crit, capacity=capacity, rule="max"))
+}
