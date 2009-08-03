09a340d9b73b68ca0ff564c78e9ac31b8f74e407
David Fencsik <david.fencsik@gmail.com>
2009-08-03 12:56:46 -0700

    Add FitMaxSSE.r to refresh list

--- crossover04/refresh.r
+++ crossover04/refresh.r
@@ -1,7 +1,8 @@
 ### Refresh all analysis files in this directory
 
 refresh <- function () {
-    targetFiles <- c("data00.r")
+    targetFiles <- c("data00.r",
+                     "FitMaxSSE.r")
 
     for (fname in targetFiles) {
         if (!file.exists(fname)) stop(paste("File", fname, "not found"))
