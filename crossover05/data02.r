566a53af9a614d7be0b584398b6a460dedb20d97
David Fencsik <david.fencsik@gmail.com>
2011-02-05 09:04:59 -0800

    Rely on new d' calculation function.
    Switch from using this script to calculate d' to the new ComputeDprime.r
    script.

--- crossover05/data02.r
+++ crossover05/data02.r
@@ -6,7 +6,9 @@
 f.data02 <- function () {
     infile <- "data01.rda"
     outfile <- "data02.rda"
+    compute.dprime.file <- "ComputeDprime.r"
     load(infile)
+    source(compute.dprime.file)
 
     ## split data
     targ <- data01$target
@@ -29,32 +31,11 @@ f.data02 <- function () {
     data02$nhits <- tp$ncor
     data02$nfa <- ta$nobs - ta$ncor
 
-    ## correct HR for p={0,1}
-    adjHR <- data02$nhits
-    index <- adjHR == 0
-    if (any(index)) adjHR[index] <- 0.5
-    index <- adjHR == data02$npos
-    if (any(index)) adjHR[index] <- data02$npos[index] - 0.5
-    adjHR <- adjHR / data02$npos
-
-    ## correct CR for p={0,1}
-    adjFA <- data02$nfa
-    index <- adjFA == 0
-    if (any(index)) adjFA[index] <- 0.5
-    index <- adjFA == data02$nneg
-    if (any(index)) adjFA[index] <- data02$nneg[index] - 0.5
-    adjFA <- adjFA / data02$nneg
-
-    ## compute d' and criterion
-    data02$dprime <- qnorm(adjHR) - qnorm(adjFA)
-    data02$crit <- -0.5 * (qnorm(adjHR) + qnorm(adjFA))
-
-    ## compute CI around d'
-    phiFA <- 1 / sqrt(2 * pi) * exp(-0.5 * qnorm(adjFA))
-    phiHR <- 1 / sqrt(2 * pi) * exp(-0.5 * qnorm(adjHR))
-    data02$dpci <- with(data02,
-                        1.96 * sqrt(adjHR * (1 - adjHR) / npos / (phiHR^2) +
-                                    adjFA * (1 - adjFA) / nneg / (phiFA^2)))
+    ## compute d', criterion, and CIs
+    out <- with(data02, ComputeDprime(nhits, nfa, npos, nneg))
+    data02$dprime <- out$dprime
+    data02$crit <- out$criterion
+    data02$dpci <- out$ci
 
     ## save data
     save(data02, file=outfile)
