ff4d2d51fc135c88ca8c077abcd4c69027553899
David Fencsik <david.fencsik@gmail.com>
2011-05-16 21:11:15 -0700

    Remove unnecessary comments and make corrections.
     - Minor correction: remove unused argument to melt that is available in
       reshape2, but not in reshape.
     - Remove comments about moving reshape2 up in the search list.  Even if
       you do this, R ends up calling the melt.data.frame method from reshape.
       This probably should be reported.

--- crossover05/tab0303.r
+++ crossover05/tab0303.r
@@ -1,21 +1,15 @@
-### tab0303.r: test effects of setsize on criterion for limited-capacity
+### tab0303.r: test effects of setsize on criterion for unlimited-capacity
 ### models
 
 f.tab0303 <- function () {
     infile <- "data03.rda"
     outfile <- "tab0303.txt"
     library("ez", quietly=TRUE, warn.conflicts=FALSE)
-    ### ## The ez package loads reshape2, then ggplot2, which loads reshape in
-    ### ## front of reshape2, so I lose that functionality.  The following
-    ### ## three lines move reshape2 to the front of the line, but it's hackish
-    ### if (any(search() == "package:reshape2"))
-    ###     detach("package:reshape2", force=TRUE)
-    ### library("reshape2", quietly=TRUE, warn.conflicts=FALSE)
     load(infile)
     on.exit(while (sink.number() > 0) sink())
     dt <- melt(data03$par, id.vars=c("sub", "cond"),
                measure.vars=c("c1", "c2", "c4", "c8"),
-               variable_name="setsize", value.name="criterion")
+               variable_name="setsize")
     dt$setsize <- factor(sub("c", "", as.character(dt$setsize)))
     names(dt)[names(dt) == "value"] <- "criterion"
     sink(outfile)
--- crossover05/tab0403.r
+++ crossover05/tab0403.r
@@ -1,27 +1,15 @@
 ### tab0403.r: test effects of setsize on criterion for limited-capacity
 ### models
 
-### dt <- melt(data03$par, c("sub", "cond"), c("c1", "c2", "c4", "c8"),
-### variable.name="setsize", value.name="criterion")
-
-### print(ezANOVA(data=dt[dt$cond == "Orientation", ], dv=.(criterion),
-### wid=.(sub), within=.(setsize)))
-
 f.tab0403 <- function () {
     infile <- "data04.rda"
     outfile <- "tab0403.txt"
     library("ez", quietly=TRUE, warn.conflicts=FALSE)
-    ### ## The ez package loads reshape2, then ggplot2, which loads reshape in
-    ### ## front of reshape2, so I lose that functionality.  The following
-    ### ## three lines move reshape2 to the front of the line, but it's hackish
-    ### if (any(search() == "package:reshape2"))
-    ###     detach("package:reshape2", force=TRUE)
-    ### library("reshape2", quietly=TRUE, warn.conflicts=FALSE)
     load(infile)
     on.exit(while (sink.number() > 0) sink())
     dt <- melt(data04$par, id.vars=c("sub", "cond"),
                measure.vars=c("c1", "c2", "c4", "c8"),
-               variable_name="setsize", value.name="criterion")
+               variable_name="setsize")
     dt$setsize <- factor(sub("c", "", as.character(dt$setsize)))
     names(dt)[names(dt) == "value"] <- "criterion"
     sink(outfile)
