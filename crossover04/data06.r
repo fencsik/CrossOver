fd044d0a913e8190983629f16fece4d7a9c4a35b
David Fencsik <david.fencsik@gmail.com>
2011-05-17 21:40:03 -0700

    Add fixed-criterion models.
    Add functions to fit unlimited- and limited-capacity models to the data,
    and generate plots of the model to individual subject data.

---
+++ crossover04/data06.r
@@ -0,0 +1,128 @@
+### data06.r: fits a max-rule model to the data.
+###
+### Assumptions: limited capacity, variable criterion
+###
+### Fit Procedure: maximum likelihood
+
+f.data06 <- function () {
+    infile <- "data02.rda"
+    outfile <- "data06.rda"
+    max.rule.file <- "maxrule.r"
+    log.like.file <- "logLikeBinom.r"
+    compute.dprime.file <- "ComputeDprime.r"
+    load(infile)
+    source(max.rule.file)
+    source(log.like.file)
+    source(compute.dprime.file)
+    starting.k <- list(list(AES=4, CAT=3, EMP=4, KWP=3, RER=3),
+                       list(AES=4, CAT=4, EMP=4, KWP=4, RER=4))
+    names(starting.k) <- c("2v5", "Orientation")
+
+### Prepare data and output
+
+    if (is.factor(data02$setsize))
+        data02$setsize <- as.numeric(as.character(data02$setsize))
+
+    all.subjects <- sort(levels(factor(data02$sub)))
+    all.stimsets <- sort(levels(factor(data02$cond)))
+    all.setsizes <- sort(unique(data02$setsize))
+    n.subjects <- length(all.subjects)
+    n.stimsets <- length(all.stimsets)
+    n.setsizes <- length(all.setsizes)
+
+    factors <- with(data02, list(sub, cond, setsize))
+    npos <- with(data02, tapply(npos, factors, sum))
+    nneg <- with(data02, tapply(nneg, factors, sum))
+    obse.nhits <- with(data02, tapply(nhits, factors, sum))
+    obse.nfa <- with(data02, tapply(nfa, factors, sum))
+
+    pred.nhits <- array(NA, dim(obse.nhits), dimnames(obse.nhits))
+    pred.nfa <- array(NA, dim(obse.nfa), dimnames(obse.nfa))
+
+    output <- array(NA, dim=c(n.subjects, 9, n.stimsets),
+                    dimnames=list(all.subjects,
+                      c("s", "c", "k",
+                        "iter", "code", "ml", "rmse", "r", "rsq"),
+                      all.stimsets))
+
+### Define fit function
+    GoodnessOfFit <- function (p, obse) {
+        return(-1 * logLikeBinom(obse$nhits, obse$nfa, obse$npos, obse$nneg,
+                                 all.setsizes, p[1], p[2], capacity=p[3],
+                                 correct=FALSE))
+    }
+
+### Actual fitting
+
+    for (sub in all.subjects) {
+        for (cond in all.stimsets) {
+            p <- c(3, 2, starting.k[[cond]][[sub]])
+            obse <- list(nhits=obse.nhits[sub, cond, ],
+                        nfa=obse.nfa[sub, cond, ],
+                        npos=npos[sub, cond, ],
+                        nneg=nneg[sub, cond, ])
+            o <- nlminb(p, GoodnessOfFit, obse=obse)
+
+            output[sub, "s", cond] <- o$par[1]
+            output[sub, "c", cond] <- o$par[2]
+            output[sub, "k", cond] <- o$par[3]
+            output[sub, "iter", cond] <- o$iterations
+            output[sub, "code", cond] <- o$convergence
+            output[sub, "ml", cond] <- GoodnessOfFit(o$par, obse)
+
+            ## Store predicted counts
+            pred <- maxrule(o$par[1], o$par[2],
+                            all.setsizes, o$par[3])
+            pred$nhits <- pred$hr * npos[sub, cond, ]
+            pred$nfa <- pred$fa * nneg[sub, cond, ]
+            pred.nhits[sub, cond, ] <- pred$nhits
+            pred.nfa[sub, cond, ] <- pred$nfa
+
+            output[sub, "rmse", cond] <-
+                sqrt(mean((c(pred$nhits, pred$nfa) -
+                           c(obse$nhits, obse$nfa)) ^ 2))
+            output[sub, "r", cond] <-
+                cor(c(pred$nhits, pred$nfa), c(obse$nhits, obse$nfa))
+            output[sub, "rsq", cond] <- output[sub, "r", cond] ^ 2
+        }
+    }
+
+### convert back to data frames
+    data <- as.data.frame(as.table(npos))
+    names(data) <- c("sub", "cond", "setsize", "npos")
+    data$nneg <- as.data.frame(as.table(nneg))$Freq
+    data$obse.nhits <- as.data.frame(as.table(obse.nhits))$Freq
+    data$obse.nfa <- as.data.frame(as.table(obse.nfa))$Freq
+    data$pred.nhits <- as.data.frame(as.table(pred.nhits))$Freq
+    data$pred.nfa <- as.data.frame(as.table(pred.nfa))$Freq
+    data <- data[with(data, order(sub, cond, setsize)), ]
+    rownames(data) <- 1:length(rownames(data))
+
+    ## compute observed and predicted dprime
+    data$obse.dprime <- ComputeDprime(data$obse.nhits, data$obse.nfa,
+                                      data$npos, data$nneg)$dprime
+    data$pred.dprime <- ComputeDprime(data$pred.nhits, data$pred.nfa,
+                                      data$npos, data$nneg)$dprime
+
+    ## convert output/parameters to a data frame
+    first <- TRUE
+    for (i in dimnames(output)[[2]]) {
+        if (first) {
+            param <- as.data.frame(as.table(output[, i, ]))
+            names(param) <- c("sub", "cond", i)
+            first <- FALSE
+        } else {
+            eval(parse(text=sprintf("param$%s <- as.data.frame(as.table(output[,i,]))$Freq", i)))
+        }
+    }
+    param <- param[with(param, order(cond, sub)), ]
+
+    data06 <- list(data=data, par=param,
+                rsq=with(data,
+                  cor(c(obse.nhits, obse.nfa), c(pred.nhits, pred.nfa))^2),
+                rule="max", crit="ML", type="counts")
+    save(data06, file=outfile)
+}
+
+f.data06()
+rm(f.data06)
---
+++ crossover04/fig0601.r
@@ -0,0 +1,68 @@
+### fig0601.r: plot individual observed and predicted d' as a function of
+### setsize, separated by stim set
+
+f.fig0601 <- function () {
+    infile <- "data06.rda"
+    outfile <- "fig0601.pdf"
+    ## error checking
+    on.exit(if (exists("opar")) par(opar))
+    on.exit(while (dev.cur() != 1) dev.off(), add=TRUE)
+    load(infile)
+
+    ## define plot parameters (ordering is 2v5, orientation)
+    col <- c("green4", "blue")
+    bg <- c("white", "white")
+    pch <- c(21, 22)
+    lty <- c(1, 1)
+    lwd <- c(2, 2)
+    cex <- c(1.5, 1.5)
+    lty.model <- c(1, 1)
+    lwd.model <- c(2, 2)
+    ylim <- c(0, 5)
+
+    ## open PDF file
+    pdf(outfile, width = 9, height = 6, pointsize = 12)
+    opar <- par(mfrow = c(2, 3), las = 1, pty = "m", cex.axis = .6,
+                xpd = NA, bg = "white")
+
+    ## extract data
+    obse <- with(data06$data,
+                 tapply(obse.dprime, list(setsize, cond, sub), mean))
+    pred <- with(data06$data,
+                 tapply(pred.dprime, list(setsize, cond, sub), mean))
+    x <- as.numeric(dimnames(obse)[[1]])
+    conditions <- dimnames(obse)[[2]]
+
+    ## assign names to plotting parameters
+    names(col) <- names(bg) <- names(pch) <- names(lty) <- names(lwd) <-
+        names(cex) <- names(lty.model) <- names(lwd.model) <- conditions
+
+    counter <- 0
+    for (sub in dimnames(obse)[[3]]) {
+        matplot(x, obse[, , sub], type="n", axes=F, ylim=ylim,
+                xlab="", ylab="", main=sub)
+        axis(1, x)
+        axis(2)
+        for (cond in dimnames(obse)[[2]]) {
+            lines(x, pred[, cond, sub], type="l",
+                  col=col[cond],
+                  lty=lty.model[cond], lwd=lwd.model[cond])
+            lines(x, obse[, cond, sub], type="p",
+                  col=col[cond], lty=lty[cond], lwd=lwd[cond],
+                  pch=pch[cond], bg=bg[cond], cex=cex[cond])
+        }
+        if (counter %% 6 >= 0) title(xlab = "Setsize")
+        if (counter %% 3 >= 0) title(ylab = "d'")
+        if (counter %% 6 == 2) {
+            legend("bottom", dimnames(obse)[[2]],
+                   inset=c(0, -1),
+                   bty="n", ncol=1, cex=1.0,
+                   col=col, pch=pch, pt.bg=bg, lty=lty, lwd=lwd)
+        }
+        counter <- counter + 1
+    }
+}
+
+
+f.fig0601()
+rm(f.fig0601)
